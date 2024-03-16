9780eb7e775c4221b4d16d0b1fe28256204d05eb
cycz <z.cao16@ncl.ac.uk>
2024-03-16 22:13:15 +0000

    czcy rabbitmq配置

--- subtlechat-mini-master/src/main/java/top/javahai/chatroom/controller/LoginController.java
+++ subtlechat-mini-master/src/main/java/top/javahai/chatroom/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.javahai.chatroom.config.VerificationCode;
 import top.javahai.chatroom.entity.RespBean;
+import top.javahai.chatroom.service.VerifyCodeService;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -21,12 +22,10 @@ import java.util.Random;
  */
 @RestController
 public class LoginController {
-  /**
-   * 获取验证码图片写到响应的输出流中，保存验证码到session
-   * @param response
-   * @param session
-   * @throws IOException
-   */
+
+  @Autowired
+  VerifyCodeService verifyCodeService;
+
   @GetMapping("/verifyCode")
   public void getVerifyCode(HttpServletResponse response, HttpSession session) throws IOException {
     VerificationCode code = new VerificationCode();
@@ -36,35 +35,12 @@ public class LoginController {
     VerificationCode.output(image,response.getOutputStream());
   }
 
-  @Autowired
-  JavaMailSender javaMailSender;
-  /**
-   * 获取邮箱验证码，并保存到本次会话
-   * @param session
-   */
   @GetMapping("/admin/mailVerifyCode")
   public RespBean getMailVerifyCode(HttpSession session){
-    //获取随机的四个数字
-    StringBuilder code=new StringBuilder();
-    for (int i = 0; i <4; i++) {
-      code.append(new Random().nextInt(10));
-    }
-   //邮件内容
-    SimpleMailMessage msg = new SimpleMailMessage();
-    msg.setSubject("微言聊天室管理端验证码验证");
-    msg.setText("本次登录的验证码："+code);
-    msg.setFrom("hanxrun@gmail.com");
-    msg.setSentDate(new Date());
-    msg.setTo("1031762352@qq.com");
+    String code = verifyCodeService.getVerifyCode();
     //保存验证码到本次会话
-    session.setAttribute("mail_verify_code",code.toString());
-    //发送到邮箱
-    try {
-      javaMailSender.send(msg);
-      return RespBean.ok("验证码已发送到邮箱，请注意查看！");
-    }catch (Exception e){
-      e.printStackTrace();
-      return RespBean.error("服务器出错啦！请稍后重试！");
-    }
+    session.setAttribute("mail_verify_code",code);
+    verifyCodeService.sendVerifyCodeMail(code);
+    return RespBean.ok("The verification code has been sent to the mailbox, please pay attention to check");
   }
 }
