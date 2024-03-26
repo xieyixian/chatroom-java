122bb8b723d7e759d74ce9d2737af8129753fc50
hanxrun@gmail.com <hanxrun@gmail.com>
2024-03-26 11:50:59 +0800

    change name

--- subtlechat-mini-master/src/main/java/top/team7/chatroom/service/AesEncryptService.java
+++
@@ -1,8 +0,0 @@
-package top.team7.chatroom.service;
-
-import org.springframework.stereotype.Service;
-
-public interface AesEncryptService {
-
-    public  String getKey(Long id);
-}
--- subtlechat-mini-master/src/main/java/top/team7/chatroom/service/MessageService.java
+++
@@ -1,7 +0,0 @@
-package top.team7.chatroom.service;
-import top.team7.chatroom.entity.GroupMsgContent;
-
-public interface MessageService {
-    public void SendGroupMsg(GroupMsgContent groupMsgContent);
-
-}
